e4c9f8b1dcfd8d714a2753379a73de360ad7442b
Apple <dsz>
2018-08-08 16:19:10 +0800

    no message

---
+++ DSZPhotoCropEditor/CropViewController.h
@@ -0,0 +1,19 @@
+//
+//  CropViewController.h
+//  DSZPhotoCropEditor
+//
+//  Created by Apple on 2018/8/8.
+//  Copyright © 2018年 Apple. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CropViewController.
+FOUNDATION_EXPORT double CropViewControllerVersionNumber;
+
+//! Project version string for CropViewController.
+FOUNDATION_EXPORT const unsigned char CropViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CropViewController/PublicHeader.h>
+
